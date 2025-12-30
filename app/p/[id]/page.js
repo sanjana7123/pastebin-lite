e5fa7168de17bf0011fb106e04f7288e3e23794e
sanjana7123 <garnepudisanjana@gmail.com>
2025-12-30 18:44:45 +0530

    intial pastebin lite project

--- app/p/[id]/page.js
+++ app/p/[id]/page.js
@@ -1,27 +1,43 @@
-import { redis } from "../../../lib/redis";
-import { notFound } from "next/navigation";
+async function getPaste(id) {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/pastes/${id}`,
+    { cache: "no-store" }
+  );
+
+  if (!res.ok) return null;
+  return res.json();
+}
 
 export default async function PastePage({ params }) {
-  const data = await redis.get(`paste:${params.id}`);
+  const data = await getPaste(params.id);
 
-  // If paste doesn't exist → 404
-  if (!data) notFound();
+  if (!data) {
+    return (
+      <h2 style={{ padding: 20 }}>
+        Paste not found or expired
+      </h2>
+    );
+  }
 
   return (
-    <div style={{
-      minHeight: "100vh",
-      background: "#0f172a",
-      padding: 20,
-      color: "#e5e7eb"
-    }}>
-      <pre style={{
-        maxWidth: 800,
-        margin: "0 auto",
-        background: "#020617",
+    <div
+      style={{
+        minHeight: "100vh",
+        background: "#0f172a",
         padding: 20,
-        borderRadius: 10,
-        whiteSpace: "pre-wrap"
-      }}>
+        color: "#e5e7eb",
+      }}
+    >
+      <pre
+        style={{
+          maxWidth: 800,
+          margin: "0 auto",
+          background: "#020617",
+          padding: 20,
+          borderRadius: 10,
+          whiteSpace: "pre-wrap",
+        }}
+      >
         {data.content}
       </pre>
     </div>
